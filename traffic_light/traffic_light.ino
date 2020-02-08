2dc6074c4561b11a90f7ad833880a731e1391ea2
Dani Foldi <dani@stefan.hu>
2020-02-08 22:00:19 +0100

    Added traffic light project

---
+++ traffic_light/traffic_light.ino
@@ -0,0 +1,26 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  delay(10000);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  delay(1000);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  delay(5000);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  delay(2000);
+}
